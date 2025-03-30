d6d571aa1a9c5dd4277241641db30ee79a254960
Andres Fuentes <fuentesandres31@gmail.com>
2025-03-30 12:57:09 -0300

    lista de las clases

---
+++ src/assets/clasesBento.ts
@@ -0,0 +1,12 @@
+export const clasesBento = [
+    "row-span-2 border-2 border-black",
+    "row-span-3 col-start-2 row-start-1 border-2 border-black",
+    "row-span-2 col-start-2 row-start-4 border-2 border-black",
+    "row-span-3 col-start-3 row-start-3 border-2 border-black",
+    "row-span-2 col-start-3 row-start-1 border-2 border-black",
+    "row-span-2 col-start-4 row-start-4 border-2 border-black",
+    "row-span-3 col-start-4 row-start-1 border-2 border-black",
+    "row-span-3 col-start-5 row-start-3 border-2 border-black",
+    "row-span-3 col-start-1 row-start-3 border-2 border-black",
+    "row-span-2 col-start-5 row-start-1 border-2 border-black",
+]
